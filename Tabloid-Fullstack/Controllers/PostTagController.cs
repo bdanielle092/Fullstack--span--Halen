f277760750cbb418204f1fa66a34389129b45adc
jenniferstarkey <jenniferrstarkey@yahoo.com>
2021-01-26 14:39:03 -0600

    checking out

--- Tabloid-Fullstack/Controllers/PostTagController.cs
+++ Tabloid-Fullstack/Controllers/PostTagController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Tabloid_Fullstack.Models;
+using Tabloid_Fullstack.Models.ViewModels;
+using Tabloid_Fullstack.Repositories;
+
 
 namespace Tabloid_Fullstack.Controllers
 {
@@ -11,5 +15,45 @@ namespace Tabloid_Fullstack.Controllers
     [ApiController]
     public class PostTagController : ControllerBase
     {
+
+        private readonly IPostRepository _repo;
+        private readonly IUserProfileRepository _userProfileRepository;
+        private ICommentRepository _commentRepo;
+        private readonly ITagRepository _tagRepo;
+
+
+        public PostTagController(IPostRepository repo, ITagRepository tagRepo, ICommentRepository commentRepo, IUserProfileRepository userProfileRepository)
+        {
+            _repo = repo;
+            _commentRepo = commentRepo;
+            _userProfileRepository = userProfileRepository;
+            _tagRepo = tagRepo;
+        }
+        private UserProfile GetCurrentUserProfile()
+        {
+            try
+            {
+                var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        [HttpPost]
+        public IActionResult Add(PostTag postTag)
+        {
+            var currentUser = GetCurrentUserProfile();
+            var postUserId = _repo.GetById(postTag.PostId);
+
+            if (currentUser.Id != postUserId.UserProfileId)
+            {
+                return NotFound();
+            }
+
+            _repo.AddPostTag(postTag);
+            return NoContent();
+        }
     }
 }
